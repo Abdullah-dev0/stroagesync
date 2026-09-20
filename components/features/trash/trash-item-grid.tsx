802eb1de1a51afc3d98838a25af8d35c6afdf8a0
Abdullah <abdulah14200@gmail.com>
2026-09-20 06:42:59 +0500

    chore: remove unused trash server wrapper

--- components/features/trash/trash-item-grid.tsx
+++
@@ -1,7 +0,0 @@
-import { getTrashItems } from "@/lib/queries/storage"
-import { TrashItemGridClient } from "@/components/features/trash/trash-item-grid-client"
-
-export async function TrashItemGrid() {
-  const items = await getTrashItems()
-  return <TrashItemGridClient initialItems={items} />
-}
