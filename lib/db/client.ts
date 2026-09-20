a89d009c4c4f4eead2d611c93fae1e41fea016fa
abdullah-dev0 <abdulah14200@gmail.com>
2026-09-20 17:26:10 +0500

    Refactor code structure for improved readability and maintainability

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -4,9 +4,10 @@ import * as dbSchema from "@/lib/db/schema"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { Pool } from "pg"
 import { cache } from "react"
+import { env } from "cloudflare:workers"
 
 export const getDbAsync = cache(async () => {
-  const connectionString = process.env.DATABASE_URL
+  const connectionString = env.HIPERDRIVE.connectionString
 
   if (!connectionString) {
     throw new Error("Missing environment variable: DATABASE_URL")
